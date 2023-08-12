dbb9e09ddaf53895463c4e302b9326761ab65fce
thieryw <williamalexthiery@hotmail.fr>
2023-08-13 00:30:53 +0200

    home page with column layout component.

---
+++ src/components/SexyColumnLayout.tsx
@@ -0,0 +1,81 @@
+import { memo } from "react";
+import type { ReactNode } from "react";
+import { makeStyles } from "../theme";
+
+export type SexyColumnLayoutProps = {
+	className?: string;
+	columns: {
+		nodes: ReactNode[];
+		className?: string;
+	}[];
+}
+
+export const SexyColumnLayout = memo((props: SexyColumnLayoutProps) => {
+
+	const { columns, className } = props;
+	const { classes, theme, cx } = useStyles();
+
+	const getFlexGap = (
+		direction: "column" | "row",
+	) => {
+		const value = theme.spacing(4);
+		if (direction === "column") {
+			return {
+				"marginTop": value,
+				"marginBottom": value
+			}
+		}
+		return {
+			"marginLeft": value,
+			"marginRight": value
+		}
+
+	}
+
+	return <div className={cx(classes.root, className)}>
+		{
+			columns.map(({ nodes, className }, index) => <div
+				style={{
+					...getFlexGap("row")
+				}}
+				className={cx(classes.column, className)}
+			>
+				{
+					nodes.map((node, index) => <div
+						className={classes.nodeWrapper}
+						style={{
+							...getFlexGap("column")
+						}}
+					>
+						{node}
+					</div>)
+				}
+
+
+			</div>)
+		}
+	</div>
+});
+
+const useStyles = makeStyles()(theme => {
+
+	return {
+		"root": {
+			"display": "flex",
+			"margin": -theme.spacing(4)
+		},
+		"column": {
+			"position": "relative",
+			"display": "flex",
+			"flexDirection": "column",
+			"justifyContent": "center",
+			"maxWidth": 340,
+		},
+		"nodeWrapper": {
+
+		}
+
+	}
+
+})
+
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,53 +1,122 @@
 import { useTranslation, declareComponentKeys } from "i18n";
-import { makeStyles, Text } from "theme";
+import { makeStyles, Text, breakpointsValues } from "theme";
 import { LinkButton } from "components/LinkButton";
 import { CardLink } from "components/CardLink";
+import mokImage from "assets/img/gallery/Naturalisme/Antilles/1_12/miniature_1_12.jpg";
+import mokImage2 from "assets/img/gallery/Reportages/Le-brame-du-cerf dans-les-Cévennes/23/miniature_23.jpg";
+import { SexyColumnLayout } from "components/SexyColumnLayout";
+import { routes } from "../router";
 
 export function Home() {
 	const { t } = useTranslation({ Home });
-	const { classes } = useStyles();
+	const { classes, cx } = useStyles();
 	return <div className={classes.root}>
 		<div className={classes.titleWrapper}>
-			<div className={classes.title}>
-				<div className={classes.upper}>
-					<Text className={classes.name} typo="page heading">{t("name")}</Text>
-					<Text className={classes.journalist} typo="page heading">{t("expertise1")}</Text>
-				</div>
-				<Text className={classes.ecologist} typo="page heading">{t("expertise2")}</Text>
-				<div className={classes.lower}>
-					<Text className={classes.and} typo="page heading">{t("and")}</Text>
+			<div className={classes.titleInner}>
+				<div className={classes.title}>
+					<div className={classes.upper}>
+						<Text className={classes.name} typo="page heading">{t("name")}</Text>
+						<Text className={classes.journalist} typo="page heading">{t("expertise1")}</Text>
+					</div>
+					<Text className={classes.ecologist} typo="page heading">{t("expertise2")}</Text>
+					<div className={classes.lower}>
+						<Text className={classes.and} typo="page heading">{t("and")}</Text>
 
-					<Text className={classes.photographer} typo="page heading">{t("artisticOccupation")}</Text>
-				</div>
+						<Text className={classes.photographer} typo="page heading">{t("artisticOccupation")}</Text>
+					</div>
 
-			</div>
-			<div className={classes.linkWrapper}>
-				<LinkButton
-					className={classes.authorLink}
-					label={t("authorLinkLabel")}
-					link={{
-						"href": ""
-					}}
-				/>
-				<LinkButton
-					label={t("articleLinkLabel")}
-					link={{
-						"href": ""
-					}}
-				/>
+				</div>
+				<div className={classes.linkWrapper}>
+					<LinkButton
+						className={classes.authorLink}
+						label={t("authorLinkLabel")}
+						link={{
+							...routes.auteur().link
+						}}
+					/>
+					<LinkButton
+						label={t("articleLinkLabel")}
+						link={{
+							...routes.reportage().link
+						}}
+					/>
+
+				</div>
 
 			</div>
 
 		</div>
-		<CardLink 
-			title={t("naturTitle")}
-			paragraph={t("naturParagraph")}
-			link={{
-				"label": t("exploreButton"),
-				"href": ""
-			}}
-			cardNumber="01"
-		/>
+		<div className={classes.galleryPresentation}>
+			<SexyColumnLayout
+				columns={[
+					{
+						"nodes": [
+							<img className={classes.presentationImage} src={mokImage} alt="naturalism presentation" />,
+
+						],
+						"className": classes.columnOne
+					},
+					{
+						"nodes": [
+							<img className={classes.presentationImage} src={mokImage} alt="naturalism presentation" />,
+							<CardLink
+								title={t("naturTitle")}
+								paragraph={t("naturParagraph")}
+								link={{
+									"label": t("exploreButton"),
+									...routes.naturalism().link
+								}}
+								cardNumber="01"
+							/>,
+							<img className={classes.presentationImage} src={mokImage} alt="naturalism presentation" />
+						]
+					},
+					{
+						"nodes": [
+							<img className={classes.presentationImage} src={mokImage} alt="naturalism presentation" />,
+							<img className={classes.presentationImage} src={mokImage} alt="naturalism presentation" />
+
+						],
+						"className": classes.columnThree
+					}
+				]}
+			/>
+			<SexyColumnLayout
+				className={cx(classes.sexyColumnLayout, classes.sexyColumnLayoutRest)}
+				columns={[
+					{
+						"nodes": [
+							<img className={classes.presentationImage} src={mokImage2} alt="reportage presentation" />,
+
+						],
+						"className": classes.columnOne
+					},
+					{
+						"nodes": [
+							<img className={classes.presentationImage} src={mokImage2} alt="reportage presentation" />,
+							<CardLink
+								title={t("articleTitle")}
+								paragraph={t("articleParagraph")}
+								link={{
+									"label": t("exploreButton"),
+									...routes.reportage().link
+								}}
+								cardNumber="02"
+							/>,
+							<img className={classes.presentationImage} src={mokImage2} alt="reportage presentation" />
+						]
+					},
+					{
+						"nodes": [
+							<img className={classes.presentationImage} src={mokImage2} alt="reportage presentation" />,
+							<img className={classes.presentationImage} src={mokImage2} alt="reportage presentation" />
+
+						],
+						"className": classes.columnThree
+					}
+				]}
+			/>
+		</div>
 
 	</div>
 }
@@ -56,22 +125,74 @@ export function Home() {
 const useStyles = makeStyles()(theme => {
 	const titleWordSpacing = "0.5rem"
 
+	const paddingRightLeft = (() => {
+		if (theme.windowInnerWidth >= breakpointsValues["lg+"]) {
+			return 8;
+		};
+		if (theme.windowInnerWidth >= breakpointsValues.lg && theme.windowInnerWidth < breakpointsValues["lg+"]) {
+			return 5;
+		};
+		if (theme.windowInnerWidth >= breakpointsValues.md && theme.windowInnerWidth < breakpointsValues.lg) {
+			return 8;
+		}
+		return 4;
+	})();
+
+
 	return ({
-		"root": {
-			"paddingTop": theme.spacing(11),
-			...theme.spacing.rightLeft("padding", `${theme.spacing(8)}px`)
+		"galleryPresentation": {
+			"display": "flex",
+			"flexDirection": "column",
+			"alignItems": theme.windowInnerWidth >= breakpointsValues["lg+"] || theme.windowInnerWidth < breakpointsValues.sm ? "center" : "flex-end",
+			"position": "relative",
+			"marginTop": theme.windowInnerWidth >= 819 ? -theme.spacing(14) : theme.spacing(8),
 
 		},
+		"sexyColumnLayout": {
+			...theme.spacing.topBottom("margin", `${theme.spacing(4)}px`)
+
+		},
+		"sexyColumnLayoutRest": {
+			"alignSelf": "center"
+
+		},
+		"titleInner": {
+		},
+		"presentationImage": {
+			"width": "100%",
+			"minHeight": "100%"
+
+		},
+		"columnThree": {
+			...(theme.windowInnerWidth < breakpointsValues.lg ? {
+				"display": "none"
+			} : {})
+		},
+		"columnOne": {
+			...(theme.windowInnerWidth < breakpointsValues.sm ? {
+				"display": "none"
+			} : {})
+
+		},
+		"root": {
+			"paddingTop": theme.spacing(11),
+			...theme.spacing.rightLeft("padding", `${theme.spacing(paddingRightLeft)}px`),
+		},
 		"titleWrapper": {
+			"zIndex": 300,
+			"position": "relative",
+			"display": "flex",
+			"justifyContent": theme.windowInnerWidth >= breakpointsValues.sm ? undefined : "center"
 		},
 		"title": {
 			"display": "grid",
 			"gridTemplateColumns": "1fr",
-			"gap": theme.spacing(2)
+			"gap": theme.spacing(2),
 
 		},
 		"upper": {
-			"display": "flex"
+			"display": "flex",
+			"flexWrap": "wrap"
 
 		},
 		"lower": {
@@ -117,6 +238,8 @@ export const { i18n } = declareComponentKeys<
 	"and" |
 	"exploreButton" |
 	"naturTitle" |
-	"naturParagraph" 
+	"naturParagraph" |
+	"articleTitle" |
+	"articleParagraph"
 
 >()({ Home });

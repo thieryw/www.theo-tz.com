d46ed598e3131c39b492806037d28815ed39a169
thieryw <williamalexthiery@hotmail.fr>
2023-08-15 01:18:25 +0200

    update gallery layout and breakpoints

--- src/components/Gallery.tsx
+++ src/components/Gallery.tsx
@@ -1,5 +1,6 @@
 import { ArtGallery } from "react-art-gallery";
-import {generateLightboxImgSources, generateThumbnailImgSources} from "tools/generateImgSource";
+import { generateLightboxImgSources, generateThumbnailImgSources } from "tools/generateImgSource";
+import { breakpointsValues } from "theme";
 
 type File = Record<string, {
 	files: {
@@ -24,29 +25,37 @@ export type GalleryProps = {
 
 
 export function Gallery(props: GalleryProps) {
-	const {jpg, webp} = props;
+	const { jpg, webp } = props;
 
 	return <ArtGallery
-					images={Object.values(webp).map((value, index) => {
-						return {
-							"thumbNail": {
-								"src": value.files.miniature.url,
-								"sources": generateThumbnailImgSources(value.files.miniature.url, Object.values(jpg)[index].files.miniature.url),
-								"alt": value.files.miniature.name
-							},
-							"lightBox": {
-								"src": value.files.large.url,
-								"sources": generateLightboxImgSources(
-									value.files.large.url,
-									value.files.medium.url,
-									Object.values(jpg)[index].files.large.url,
-									Object.values(jpg)[index].files.medium.url,
-								)
-							}
-						}
-					})}
-					thumbNailAlinement="vertical"
-					hideImageNames={true}
+		images={Object.values(webp).map((value, index) => {
+			return {
+				"thumbNail": {
+					"src": value.files.miniature.url,
+					"sources": generateThumbnailImgSources(value.files.miniature.url, Object.values(jpg)[index].files.miniature.url),
+					"alt": value.files.miniature.name
+				},
+				"lightBox": {
+					"src": value.files.large.url,
+					"sources": generateLightboxImgSources(
+						value.files.large.url,
+						value.files.medium.url,
+						Object.values(jpg)[index].files.large.url,
+						Object.values(jpg)[index].files.medium.url,
+					)
+				}
+			}
+		})}
+		thumbNailAlinement="vertical"
+		hideImageNames={true}
+		columnCountForVerticalAlinement={4}
+		breakpointsForColumns={{
+			"xl": breakpointsValues["xl"],
+			"lg+": breakpointsValues["lg+"],
+			"md": breakpointsValues["md"],
+			"sm": breakpointsValues.sm
+		}}
+
 
 	/>
 

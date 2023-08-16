3d887b293221161d39dd0a847f2187bc254484e2
thieryw <williamalexthiery@hotmail.fr>
2023-08-16 03:16:05 +0200

    update smoothScroll provider

--- src/components/SmoothScrollProvider.tsx
+++ src/components/SmoothScrollProvider.tsx
@@ -2,7 +2,6 @@ import { useEffect, createContext, useState, useRef } from "react";
 import type { ReactNode } from "react";
 import { getScrollableParent } from "powerhooks/getScrollableParent"
 import { useDomRect } from "powerhooks/useDomRect";
-import { makeStyles } from "theme";
 import { isTouchDevice } from "../tools/isTouchDevice";
 import { useConstCallback } from "powerhooks/useConstCallback";
 
@@ -22,7 +21,6 @@ export const ScrollContext = createContext<GlobalStateContextType | undefined>(u
 export type SmoothScrollProviderProps = {
 	children?: ReactNode;
 	className?: string;
-	classes?: Partial<ReturnType<typeof useStyles>["classes"]>;
 };
 
 
@@ -35,62 +33,118 @@ export function SmoothScrollProvider(props: SmoothScrollProviderProps) {
 	const rootRef = useRef<HTMLDivElement>(null);
 	const [globalState, setGlobalState] = useState<GlobalStateType>({ "isScrollable": true });
 	const scrollTopWhenNotIsScrollableRef = useRef<number | undefined>(undefined);
-	const isScrollingToTopRef = useRef<boolean>(false);
-
-
-	const scrollToTop = useConstCallback(() => {
-		isScrollingToTopRef.current = true;
+	const [isScrollingToTop, setIsScrollingToTop] = useState(false)
+	const transitionRef = useRef({
+		"transition": "top 500ms",
+		"timingFunction": "cubic-bezier(.15,.67,.15,.97)"
 	});
 
 
-	useEffect(function handleScrollEvent(){
-		if (isTouchDevice()) {
+	const scrollToTop = useConstCallback(() => {
+		setIsScrollingToTop(true);
+	});
+
+	useEffect(function handleNavPrevNextEvent(){
+
+    window.addEventListener('popstate', scrollToTop);
+
+    return () => {
+      window.removeEventListener('popstate', scrollToTop);
+    };
+  }, [scrollToTop]);
+
+	useEffect(function setInitialStyle(){
+		if(isTouchDevice()){
 			return;
 		}
+		const contentWrapperStyle = contentWrapperRef.current.style;
 
-		(function handleToggleIsScrollable() {
-			if (!rootRef.current || height === 0) {
-				return;
-			}
+		contentWrapperStyle.position = "fixed";
+		contentWrapperStyle.transition = transitionRef.current.transition;
+		contentWrapperStyle.transitionTimingFunction = transitionRef.current.timingFunction;
 
-			const scrollableParent = getScrollableParent({
-				"doReturnElementIfScrollable": true,
-				"element": contentWrapperRef.current
-			});
+	},[contentWrapperRef])
+	useEffect(function setHeight(){
+		if(!rootRef.current){
+			return;
+		}
+		rootRef.current.style.height = `${height}px`;
 
+	},[height])
 
-			if (!globalState.isScrollable) {
-				scrollTopWhenNotIsScrollableRef.current = scrollableParent.scrollTop;
-				rootRef.current.style.height = "100vh";
-				return;
-			}
-
-
-			rootRef.current.style.height = `${height}px`;
-
-			scrollableParent.scrollTo({
-				"top": (() => {
-					if (isScrollingToTopRef.current) {
-						isScrollingToTopRef.current = false;
-						contentWrapperRef.current.style.top = "0px";
-						return 0;
-					}
-					return scrollTopWhenNotIsScrollableRef.current;
-				})()
-			});
-			scrollTopWhenNotIsScrollableRef.current = undefined;
-
-		}())
-
+	useEffect(function handleScrollToTop(){
+		if(!isScrollingToTop || !rootRef.current){
+			return;
+		};
+		contentWrapperRef.current.style.transition = "none";
+		contentWrapperRef.current.style.transitionTimingFunction = "none";
 		const scrollableParent = getScrollableParent({
 			"doReturnElementIfScrollable": true,
 			"element": contentWrapperRef.current
 		});
+		if(rootRef.current.style.height === "100vh"){
+			rootRef.current.style.height = `${height}px`;
+		}
 
+		contentWrapperRef.current.style.top = "0px";
+		scrollableParent.scrollTo({
+			"top": 0
+		});
+		scrollTopWhenNotIsScrollableRef.current = 0;
 
-		if (!globalState.isScrollable) {
+		setIsScrollingToTop(false);
+
+	},[isScrollingToTop, contentWrapperRef, height]);
+
+	useEffect(function resetTransitionsAfterScrollTop(){
+		if(isScrollingToTop){
 			return;
 		}
+		contentWrapperRef.current.style.transition = transitionRef.current.transition;
+		contentWrapperRef.current.style.transitionTimingFunction = transitionRef.current.timingFunction;
+
+	},[isScrollingToTop, contentWrapperRef])
+
+	useEffect(function handleToggleIsScrollable(){
+		if(isScrollingToTop){
+			return;
+		}
+		if (!rootRef.current || height === 0) {
+			return;
+		}
+		const scrollableParent = getScrollableParent({
+			"doReturnElementIfScrollable": true,
+			"element": contentWrapperRef.current
+		});
+		if (!globalState.isScrollable) {
+			scrollTopWhenNotIsScrollableRef.current = scrollableParent.scrollTop;
+			rootRef.current.style.height = "100vh";
+			if(isTouchDevice()){
+				rootRef.current.style.overflow = "hidden";
+			};
+			return;
+		}
+		if(isTouchDevice()){
+			rootRef.current.style.overflow = "visible";
+		};
+		rootRef.current.style.height = `${height}px`;
+		scrollableParent.scrollTo({
+			"top": scrollTopWhenNotIsScrollableRef.current
+		});
+		scrollTopWhenNotIsScrollableRef.current = undefined;
+
+	// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, [contentWrapperRef, globalState.isScrollable, isScrollingToTop])
+
+
+	useEffect(function handleScrollEvent() {
+		if (isTouchDevice() || !globalState.isScrollable) {
+			return;
+		}
+		const scrollableParent = getScrollableParent({
+			"doReturnElementIfScrollable": true,
+			"element": contentWrapperRef.current
+		});
 
 		const handleScroll = () => {
 			contentWrapperRef.current.style.top = `-${scrollableParent.scrollTop}px`;
@@ -101,53 +155,16 @@ export function SmoothScrollProvider(props: SmoothScrollProviderProps) {
 		return () => {
 			scrollableParent.removeEventListener("scroll", handleScroll)
 		}
-
-
-	}, [contentWrapperRef, height, globalState.isScrollable])
-
-	const { classes } = useStyles({
-		height,
-		"isScrollable": globalState.isScrollable,
-		"isTouchDevice": isTouchDevice(),
-		"isScrollingToTop": isScrollingToTopRef.current,
-
-	}, { props });
+	}, [contentWrapperRef, globalState.isScrollable])
 
 	return <ScrollContext.Provider value={{ globalState, setGlobalState, scrollToTop }} >
-		<div ref={rootRef} className={classes.root}>
-			<div ref={contentWrapperRef} className={classes.contentWrapper}>
+		<div ref={rootRef}>
+			<div ref={contentWrapperRef}>
 				{
 					children
 				}
-
 			</div>
-
 		</div>
 	</ScrollContext.Provider>
 
 }
-
-
-const useStyles = makeStyles<{
-	height: number;
-	isScrollable: boolean;
-	isTouchDevice: boolean;
-	isScrollingToTop: boolean;
-}>()((...[, { height, isScrollable, isTouchDevice, isScrollingToTop }]) => {
-	return ({
-		"root": {
-			...(isTouchDevice ? {} : {
-				height,
-				"overflow": isScrollable ? undefined : "hidden"
-			}),
-
-		},
-		"contentWrapper": {
-			...(isTouchDevice ? {} : {
-				"position": "fixed",
-				"transition": isScrollingToTop ? "none" : "top 500ms",
-				"transitionTimingFunction": isScrollingToTop ? "none" : "cubic-bezier(.15,.67,.15,.97)"
-			})
-		}
-	})
-});

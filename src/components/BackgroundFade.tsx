088302a86297bcd050956809aa441c44895d5a99
thieryw <39378411+thieryw@users.noreply.github.com>
2023-10-13 18:46:48 +0000

    put correct width on BackgroundFade component

--- src/components/BackgroundFade.tsx
+++ src/components/BackgroundFade.tsx
@@ -41,7 +41,7 @@ const useStyles = makeStyles<{
 }>()(
 	(theme, {backgroundImageUrl, fadeDirection}) => ({
 		"root": {
-			"width": theme.windowInnerWidth,
+			"width": "100%",
 			"height": "100%",
 			"position": "absolute",
 			"top": 0,
